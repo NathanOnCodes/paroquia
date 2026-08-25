c01946c4ba599373af4a0f75382e4df69a4d38f0
NathanOnCodes <contatonathancf@gmail.com>
2026-08-24 22:14:36 -0300

    refactor: proxy usa novos nomes de variáveis

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,8 +6,8 @@ export async function proxy(request: NextRequest) {
   const response = NextResponse.next({ request });
 
   const supabase = createServerClient(
-    env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    env.NEXT_PUBLIC_URL_SUPABASE,
+    env.NEXT_PUBLIC_CHAVE_PUBLICA_SUPABASE,
     {
       cookies: {
         getAll() {
